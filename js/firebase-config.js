5cb0801da5083f4bd2e53f2a48c791e7a288ffc0
Cetc2211 <mpceciliotopetecruz@gmail.com>
2026-04-16 20:47:57 -0700

    Refactor Firebase config for cleaner exports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,7 @@
-// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Tu configuración de Firebase vinculada a Academic Tracker Pro
 const firebaseConfig = {
     apiKey: "AIzaSyCDy-W8_3sB3WS8gVKZuzV_P6PdG1tBOUc",
     authDomain: "academic-tracker-qeoxi.firebaseapp.com",
@@ -13,10 +11,6 @@ const firebaseConfig = {
     appId: "1:674671663079:web:96e8125d02652b024469e3"
 };
 
-// Inicializamos la App y los servicios
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-// Exportamos para que otros archivos JS puedan usar la base de datos
-export { db, storage };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
